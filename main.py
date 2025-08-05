c3568a8c37627a81469032390d5d7ef8b217d484
Seu Nome <seu@email.com>
2025-08-05 16:55:26 +0200

    Ajuste de data com validação DD/MM/AAAA

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
+
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from datetime import datetime
 from api_kiwi import buscar_voos_kiwi
 from api_skyscanner import buscar_voos_skyscanner
-from datetime import datetime
 
 app = FastAPI()
 
-# Pasta para arquivos estáticos (logo, imagens, etc.)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Pasta com os arquivos HTML
 templates = Jinja2Templates(directory="templates")
 
-# Página inicial com o formulário
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Rota que recebe os dados do formulário e busca voos
 @app.post("/precos", response_class=HTMLResponse)
 async def precos(
     request: Request,
@@ -29,9 +25,9 @@ async def precos(
     data_retorno: str = Form(...)
 ):
     try:
-        # Converter data de dd/mm/yyyy para yyyy-mm-dd
-        data_partida_iso = datetime.strptime(data_partida, "%d/%m/%Y").strftime("%Y-%m-%d")
-        data_retorno_iso = datetime.strptime(data_retorno, "%d/%m/%Y").strftime("%Y-%m-%d")
+        # Corrige o formato para YYYY-MM-DD se necessário pelas APIs
+        data_partida_formatada = datetime.strptime(data_partida, "%d/%m/%Y").strftime("%Y-%m-%d")
+        data_retorno_formatada = datetime.strptime(data_retorno, "%d/%m/%Y").strftime("%Y-%m-%d")
     except ValueError:
         return templates.TemplateResponse("resultados.html", {
             "request": request,
@@ -39,11 +35,9 @@ async def precos(
             "erro": "Formato de data inválido. Use DD/MM/AAAA."
         })
 
-    # Buscar voos nas APIs
-    voos_kiwi = await buscar_voos_kiwi(origem, destino, data_partida_iso, data_retorno_iso)
-    voos_skyscanner = await buscar_voos_skyscanner(origem, destino, data_partida_iso, data_retorno_iso)
+    voos_kiwi = await buscar_voos_kiwi(origem, destino, data_partida_formatada, data_retorno_formatada)
+    voos_skyscanner = await buscar_voos_skyscanner(origem, destino, data_partida_formatada, data_retorno_formatada)
 
-    # Combinar resultados
     voos = voos_kiwi + voos_skyscanner
 
     return templates.TemplateResponse("resultados.html", {
